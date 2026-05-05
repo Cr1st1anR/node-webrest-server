d8148ae2533f1e75c2a67e22cea2a398e86e3e1a
Cristhian Rengifo <cristhian860@hotmail.com>
2026-05-04 21:59:05 -0500

    Agregar lógica para actualizar tareas utilizando UpdateTodoDto y refactorizar el controlador de tareas

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../../data/postgres";
-import { CreateTodoDto } from "../../domain/dtos";
-
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 
 
 export class TodosController {
@@ -42,7 +41,8 @@ export class TodosController {
 
     public updateTodo = async (req: Request, res: Response) => {
         const id = +req.params.id;
-        if (isNaN(id)) return res.status(400).json({ error: 'ID argument is not a number' });
+        const [error, updateTodoDto] = UpdateTodoDto.create({...req.body, id });
+        if (error) return res.status(400).json({ error });
 
         const todo = await prisma.todo.findFirst({
             where: { id }
@@ -50,15 +50,10 @@ export class TodosController {
 
         if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found` });
 
-        const { text, completedAt } = req.body;
         const updateTodo = await prisma.todo.update({
             where: { id },
-            data: {
-                text,
-                completedAt: (completedAt) ? new Date(completedAt) : null
-            }
+            data: updateTodoDto!.values
         });
-
 
         res.json(updateTodo);
 
